7158beb0ba2142c7e76fc5f47029abe815c77393
Hayyim <b2banalytica@gmail.com>
2020-12-12 16:16:26 +0300

    Initial Commit

---
+++ FestivalOfLights/FestivalOfLightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FestivalOfLightsApp.swift
+//  FestivalOfLights
+//
+//  Created by vitasiy on 12.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct FestivalOfLightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
